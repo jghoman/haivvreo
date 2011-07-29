3ec1e39aab0206c89e2ab95ab86935527bd0f749
Jakob Homan <jghoman@gmail.com>
2011-07-29 15:10:24 -0700

    Handle differing schemas during joins correctly when optimizing reader and writer schema resolution.

--- src/main/java/com/linkedin/haivvreo/AvroGenericRecordReader.java
+++ src/main/java/com/linkedin/haivvreo/AvroGenericRecordReader.java
@@ -21,6 +21,8 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.FsInput;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.plan.MapredWork;
 import org.apache.hadoop.hive.ql.plan.PartitionDesc;
@@ -29,7 +31,6 @@ import org.apache.hadoop.mapred.*;
 
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -38,6 +39,7 @@ import java.util.Properties;
  * as the value of the k-v pair, as Hive requires.
  */
 public class AvroGenericRecordReader implements RecordReader<NullWritable, AvroGenericRecordWritable>, JobConfigurable {
+  private static final Log LOG = LogFactory.getLog(AvroGenericRecordReader.class);
 
   final private org.apache.avro.file.FileReader<GenericRecord> reader;
   final private long start;
@@ -47,13 +49,17 @@ public class AvroGenericRecordReader implements RecordReader<NullWritable, AvroG
   public AvroGenericRecordReader(JobConf job, FileSplit split, Reporter reporter) throws IOException {
     this.jobConf = job;
     Schema latest;
+
     try {
-      latest = getSchema(job);
+      latest = getSchema(job, split);
     } catch (HaivvreoException e) {
       throw new IOException(e);
     }
+
     GenericDatumReader<GenericRecord> gdr = new GenericDatumReader<GenericRecord>();
+
     if(latest != null) gdr.setExpected(latest);
+
     this.reader = new DataFileReader<GenericRecord>(new FsInput(split.getPath(), job), gdr);
     this.reader.sync(split.getStart());
     this.start = reader.tell();
@@ -64,34 +70,44 @@ public class AvroGenericRecordReader implements RecordReader<NullWritable, AvroG
    * Attempt to retrieve the reader schema.  Haivvreo has a couple opportunities
    * to provide this, depending on whether or not we're just selecting data
    * or running with a MR job.
-   * @param job
    * @return  Reader schema for the Avro object, or null if it has not been provided.
    * @throws HaivvreoException
    */
-  private Schema getSchema(JobConf job) throws HaivvreoException, IOException {
+  private Schema getSchema(JobConf job, FileSplit split) throws HaivvreoException, IOException {
     // In "select * from table" situations (non-MR), Haivvreo can add things to the job
+    // It's safe to add this to the job since it's not *actually* a mapred job.
+    // Here the global state is confined to just this process.
     String s = job.get(AvroSerDe.HAIVVREO_SCHEMA);
     if(s != null) return Schema.parse(s);
 
     // Inside of a MR job, we can pull out the actual properties
     if(Utilities.getHiveJobID(job) != null) {
       MapredWork mapRedWork = Utilities.getMapRedWork(job);
-      LinkedHashMap<String,PartitionDesc> aliasToPartnInfo = mapRedWork.getAliasToPartnInfo();
-      // Assume that one of the partitions will point back to the table itself.
-      // I can't find anything in the Hive code that contradicts this...
-      for (PartitionDesc pd : aliasToPartnInfo.values()) {
-        Properties props = pd.getProperties();
-        if(props.containsKey(HaivvreoUtils.SCHEMA_LITERAL) || props.containsKey(HaivvreoUtils.SCHEMA_URL)) {
-          Schema schema = HaivvreoUtils.determineSchemaOrThrowException(props);
-          // while we're here, let's stash the schema in the jobconf for anyone coming after us
-          job.set(AvroSerDe.HAIVVREO_SCHEMA, schema.toString(false));
-          return schema;
+
+      // Iterate over the Path -> Partition descriptions to find the partition
+      // that matches our input split.
+      for (Map.Entry<String,PartitionDesc> pathsAndParts: mapRedWork.getPathToPartitionInfo().entrySet()){
+        String partitionPath = pathsAndParts.getKey();
+        if(pathIsInPartition(split, partitionPath)) {
+          if(LOG.isInfoEnabled()) LOG.info("Matching partition " + partitionPath + " with input split " + split);
+
+          Properties props = pathsAndParts.getValue().getProperties();
+          if(props.containsKey(HaivvreoUtils.SCHEMA_LITERAL) || props.containsKey(HaivvreoUtils.SCHEMA_URL)) {
+            return HaivvreoUtils.determineSchemaOrThrowException(props);
+          } else
+            return null; // If it's not in this property, it won't be in any others
         }
       }
+      if(LOG.isInfoEnabled()) LOG.info("Unable to match filesplit " + split + " with a partition.");
     }
     // No more places to get the schema from. Give up.  May have to re-encode later.
     return null;
   }
+
+  private boolean pathIsInPartition(FileSplit split, String partitionPath) {
+    return split.getPath().toString().startsWith(partitionPath);
+  }
+
 
   @Override
   public boolean next(NullWritable nullWritable, AvroGenericRecordWritable record) throws IOException {
