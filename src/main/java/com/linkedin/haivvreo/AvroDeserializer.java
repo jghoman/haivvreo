1dd74e344383ee93b53b4479bfcdd21688421c7b
Jakob Homan <jghoman@gmail.com>
2012-01-06 16:16:22 -0800

    HAIVVREO #8: Moving re-rencoding statement to debug level.

--- src/main/java/com/linkedin/haivvreo/AvroDeserializer.java
+++ src/main/java/com/linkedin/haivvreo/AvroDeserializer.java
@@ -115,7 +115,7 @@ class AvroDeserializer {
 
     // Check if we're working with an evolved schema
     if(!r.getSchema().equals(readerSchema)) {
-      LOG.info("Received different schemas.  Have to re-encode: " + r.getSchema().toString(false));
+      LOG.debug("Received different schemas.  Have to re-encode: " + r.getSchema().toString(false));
       if(reEncoder == null) reEncoder = new SchemaReEncoder();
       r = reEncoder.reencode(r, readerSchema);
     }
